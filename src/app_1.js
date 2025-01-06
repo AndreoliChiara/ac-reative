19246774efe788328493ef64949c8196c43b6090
AndreoliChiara <chiara.andreoli.2002@gmail.com>
2025-01-06 17:46:34 +0100

    creo pagina

---
+++ src/app_1.js
@@ -0,0 +1,20 @@
+
+//Data corrente
+
+
+// var dataCorrente = new Date();
+// // Ottenere i componenti della data (giorno, mese, anno)
+// var giorno = dataCorrente.getDate();
+// var mese = dataCorrente.getMonth() + 1; // Mesi iniziano da zero, quindi aggiungi 1
+// var anno = dataCorrente.getFullYear();
+// var ore = dataCorrente.getHours();
+// var minuti = dataCorrente.getMinutes();
+// var secondi = dataCorrente.getSeconds();
+
+// var dataFormattata = anno + '-' + (mese < 10 ? '0' + mese : mese) + '-' + (giorno < 10 ? '0' + giorno : giorno) + ' ' +
+//     (ore < 10 ? '0' + ore : ore) + ':' + (minuti < 10 ? '0' + minuti : minuti) + ':' + (secondi < 10 ? '0' + secondi : secondi);
+
+// // Inserire la data nella pagina HTML
+// document.body.innerHTML += '<p>Data corrente: ' + dataFormattata + '</p>';
+
+
